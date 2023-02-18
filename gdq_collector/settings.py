c411dc6a930182b74c3b7151a4d7420a842eb31e
Alexander Frisch <ali@crunchtime.dev>
2023-02-18 12:06:23 +0100

    change settings for esa

--- gdq_collector/settings.py
+++ gdq_collector/settings.py
@@ -1,28 +1,32 @@
 TWITTER_TAGS = [
-    "agdq2023",
-    "gamesdonequick.com",
-    "agdq",
-    "sgdq",
-    "gamesdonequick",
-    "awesome games done quick",
-    "games done quick",
-    "summer games done quick",
-    "gdq",
+    "esawinter23"
+    "esawinter2023",
+    "esamarathon",
+    "esamarathon.com",
+    "roadtoesa",
+    "insideesa",
+    "esa2023",
+    "european speedrun assembly",
 ]
-TWITCH_CHANNEL = "gamesdonequick"
+TWITCH_CHANNEL = "esamarathon"
 TWITCH_HOST = "irc.twitch.tv"
 TWITCH_PORT = 6667
 
 # Update this value to change the current event:
-EVENT_SHORTHAND = "AGDQ2023"
+EVENT_SHORTHAND = "ESAWINTER2023"
 
 # The following should stay pretty stable between events
 DONATION_URL = (
-    "https://gamesdonequick.com/tracker/event/{}".format(EVENT_SHORTHAND)
+     "https://donations.esamarathon.com/index/esaw23s1"
 )
+#    "https://gamesdonequick.com/tracker/event/{}".format(EVENT_SHORTHAND)
 SCHEDULE_URL = "https://gamesdonequick.com/schedule"
 DONATION_INDEX_URL = (
-    "https://gamesdonequick.com/tracker/donations/{}".format(EVENT_SHORTHAND)
+     "https://donations.esamarathon.com/donations/esaw23s1"
 )
-DONATION_DETAIL_URL = "https://gamesdonequick.com/tracker/donation"
-DONOR_URL = "https://gamesdonequick.com/tracker/donor"
+# "https://gamesdonequick.com/tracker/donations/{}".format(EVENT_SHORTHAND)
+DONATION_DETAIL_URL = "https://donations.esamarathon.com/donation"
+#DONATION_DETAIL_URL = "https://gamesdonequick.com/tracker/donation"
+DONOR_URL = "https://donations.esamarathon.com/donor"
+
+#DONOR_URL = "https://gamesdonequick.com/tracker/donor"
